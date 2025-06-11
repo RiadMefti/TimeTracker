62da7150bfb291d6fae0751b8ba0ce145056a830
Riad Mefti <104847379+RiadMefti@users.noreply.github.com>
2025-06-10 22:28:26 -0400

    feat: update Firebase service initialization to use FIREBASE_CREDENTIALS_JSON environment variable

--- back-end/services/firebaseSerice.go
+++ back-end/services/firebaseSerice.go
@@ -3,14 +3,25 @@ package services
 import (
 	"context"
 	"fmt"
+	"os"
 
 	firebase "firebase.google.com/go"
-
 	"google.golang.org/api/option"
 )
 
 func NewFirebaseService() (*firebase.App, error) {
-	opt := option.WithCredentialsFile("key.json")
+
+	var opt option.ClientOption
+
+	// Option 2: Use FIREBASE_CREDENTIALS_JSON environment variable
+	// This should contain the entire JSON content as a string
+	credentialsJSON := os.Getenv("FIREBASE_CREDENTIALS_JSON")
+	if credentialsJSON != "" {
+		opt = option.WithCredentialsJSON([]byte(credentialsJSON))
+	} else {
+		return nil, fmt.Errorf("firebase credentials not found. Set either GOOGLE_APPLICATION_CREDENTIALS or FIREBASE_CREDENTIALS_JSON environment variable")
+	}
+
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		return nil, fmt.Errorf("error initializing app: %v", err)
